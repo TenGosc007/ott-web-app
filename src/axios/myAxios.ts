4a3d02978961a74716f7a0bef396eb94808dd1bb
Olek <aleksander.atamanczuk@gmail.com>
2021-07-27 22:43:46 +0200

    refactor: axios

--- src/axios/myAxios.ts
+++ src/axios/myAxios.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://thebetter.bsgroup.eu",
   headers: {
+    "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
   },
 });
