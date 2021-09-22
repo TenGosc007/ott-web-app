c484f7a5e2286192b6f64483f5e42e951f454670
Aleksander Atamańczuk <a.atamanczuk@bsgroup.eu>
2021-09-22 19:41:02 +0200

    analytics

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,22 @@
 // import React, { useEffect } from "react";
-// import ReactGA from "react-ga";
+import ReactGA from "react-ga";
 
 import Layout from "components/Layout";
 import Player from "components/Player";
 import MovieList from "components/MovieList";
 
 const Home = () => {
+  const clickHandler = () => {
+    ReactGA.event({
+      category: "Button",
+      action: "Click the button form Page",
+    });
+    alert("Send the information to google analytics");
+  };
+
   return (
     <Layout title="Home">
+      <button onClick={clickHandler}>CLICK ME</button>
       <div className="home">
         <Player />
         <MovieList listId={2} />
